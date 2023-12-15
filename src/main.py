dac8d15c1f9147562eb7f824be0a099ace0c9690
Yaryna Kos <117029435+yarynakos@users.noreply.github.com>
2023-12-15 10:29:48 +0200

    update code

--- src/main.py
+++ src/main.py
@@ -2,29 +2,21 @@ import math
 
 
 def find_the_longest_wire(distance, list_of_pillar):
-    length = 0
-    if len(list_of_pillar) <= 50:
-        is_element_limit = True
-        for i in list_of_pillar:
-            if not (1 <= i <= 100):
-                is_element_limit = False
-                break
-        if is_element_limit:
-            is_last_was_changed_to_one = False
-            for index, i in enumerate(list_of_pillar):
-                if index != len(list_of_pillar) - 1:
-                    if is_last_was_changed_to_one:
-                        if list_of_pillar[index + 1] > 1:
-                            length += math.sqrt((list_of_pillar[index + 1] - 1) ** 2 + distance ** 2)
-                        else:
-                            length += distance
-                        is_last_was_changed_to_one = False
-                    else:
-                        with_no_changes = math.sqrt((list_of_pillar[index + 1] - i) ** 2 + distance ** 2)
-                        with_changes = math.sqrt((1 - i) ** 2 + distance ** 2)
-                        if with_changes > with_no_changes:
-                            is_last_was_changed_to_one = True
-                            length += with_changes
-                        else:
-                            length += with_no_changes
-    return length
+    longest_to_top = 0
+    longest_to_bottom = 0
+    current_pole = 0
+
+    while current_pole < len(list_of_pillar) - 1:
+        top_to_bottom = longest_to_top + math.sqrt(distance ** 2 + (list_of_pillar[current_pole] - 1) ** 2)
+        bottom_to_bottom = longest_to_bottom + distance
+
+        bottom_to_top = longest_to_bottom + math.sqrt(distance ** 2 + (list_of_pillar[current_pole + 1] - 1) ** 2)
+        top_to_top = longest_to_top + math.sqrt(
+            distance ** 2 + abs(list_of_pillar[current_pole] - list_of_pillar[current_pole + 1]) ** 2)
+
+        longest_to_top = max(bottom_to_top, top_to_top)
+        longest_to_bottom = max(bottom_to_bottom, top_to_bottom)
+
+        current_pole += 1
+
+    return max(longest_to_top, longest_to_bottom)
--- tests/test.py
+++ tests/test.py
@@ -22,3 +22,10 @@ class TestFindLongestWire(unittest.TestCase):
         pillar_list = []
         result = find_the_longest_wire(distance, pillar_list)
         self.assertEqual(result, 0)
+
+    def test_short(self):
+        distance = 4
+        pillar_list = [56, 18, 17, 94, 23, 7, 21, 94, 29, 54, 44, 26, 86, 79, 4, 15, 5, 91, 25, 17, 88, 66, 28, 2, 95,
+                       97, 60, 93, 40, 70, 75, 48, 38, 51, 34, 52, 87, 8, 62, 77, 35, 52, 3, 93, 34, 57, 51, 11, 39, 72]
+        result = find_the_longest_wire(distance, pillar_list)
+        self.assertAlmostEqual(result, 2738.18, delta=0.01)
